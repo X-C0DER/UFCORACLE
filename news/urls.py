68a55286c5906b7ee03e393049bf3d811182c883
Michael Alem <salazaralem@gmail.com>
2024-02-21 16:24:23 +0300

    test template

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-        path("", views.news, name="News"),
+        path('', views.index, name="Index"),
+        path ('news',views.news,name="News"),
+        path ('events',views.events,name="Events"),
+        path ('oracle',views.oracle,name="Oracle")
     ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from  django.http import HttpResponse
 
+
 # Create your views here.
+def index(request):
+    return render(request,"test.html" )
 def news(request):
-    return render(request,"index.html" )
+    return render(request,"news.html")
+def events(request):
+    return render(request,"events_oracle.html")
+def oracle(request):
+    return render(request,"events_oracle.html")
